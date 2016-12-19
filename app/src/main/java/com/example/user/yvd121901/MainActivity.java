6b0bab01d283d84f9c0e9f717aaabbf36113c822
Shaojung <shaojung@gmail.com>
2016-12-19 14:09:15 +0800

    use BroadcaseReceiver

--- app/src/main/java/com/example/user/yvd121901/MainActivity.java
+++ app/src/main/java/com/example/user/yvd121901/MainActivity.java
@@ -5,12 +5,14 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     NotificationManager nm;
+    MyReceiver receiver;
     Intent it;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         it = new Intent(MainActivity.this, MyService.class);
+        receiver = new MyReceiver();
     }
 
     public void clickShow(View v)
@@ -43,5 +46,17 @@ public class MainActivity extends AppCompatActivity {
     public void clickStop(View v)
     {
         stopService(it);
+    }
+
+    public void clickOn(View v)
+    {
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(receiver, intentFilter);
+    }
+
+    public void clickOff(View v)
+    {
+        unregisterReceiver(receiver);
     }
 }
